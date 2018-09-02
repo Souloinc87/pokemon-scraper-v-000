d726d3f2af36055adeaeae2504f054c45991e27b
Marcus  = Soulo <souloinc87@gmail.com>
2018-09-02 13:17:58 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,8 +15,8 @@ class Pokemon
     @@all
   end
 
-  def self.save
-
-  end 
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+  end
 
 end
