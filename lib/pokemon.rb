b151b207746ffcbb9c94674d5db85958e1da4408
Marcus  = Soulo <souloinc87@gmail.com>
2018-09-02 13:41:11 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,6 +25,6 @@ class Pokemon
   end
 
   def hp_alter(updated_hp, db)
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", updated_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", updated_hp, self.id)
   end
 end
